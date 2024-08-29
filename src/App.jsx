b63922cbfc8d7d5714e0868679af8df1ab45e0c2
SofiaGenchi <sofigen16@gmail.com>
2024-08-29 18:22:57 -0300

    Update SocialMedia component with modern design and CSS styling

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import './App.css'
 import NavBar from './components/NavBar/NavBar'
+import SocialMedia from './components/SocialMedia/SocialMedia'
 
 function App() {
   return (
     <>
       <div className='App'>
         <NavBar/>
+        <SocialMedia/>
       </div>
     </>
   )
