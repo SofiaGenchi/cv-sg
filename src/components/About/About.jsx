5847c83fd628a494dcd1c277ee1dfab87fcfd17a
SofiaGenchi <sofigen16@gmail.com>
2024-09-05 14:46:52 -0300

    mejorar la gestión de imágenes

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,55 +1,65 @@
 import React from 'react';
 import './About.css'; // Asegúrate de que el archivo CSS esté en la misma carpeta
 
+// Importación de imágenes
+import img7 from '../../assets/img/img7.jpg';
+import certificadoreactjs from '../../assets/certificate/certificadoreactjs.png';
+import certificadodesarrolloweb from '../../assets/certificate/certificadodesarrolloweb.png';
+import certificadojavascript from '../../assets/certificate/certificadojavascript.png';
+import certificadonodejs from '../../assets/certificate/certificadonodejs.jpg';
+import certificadoexcel from '../../assets/certificate/certificadoexcel.jpg';
+import certificadoresponsive from '../../assets/certificate/certificadoresponsive.png';
+import certificadoingles from '../../assets/certificate/certificadoingles.png';
+
 const certifications = [
   {
     id: 1,
     title: 'React Frontend Development Program',
     description: 'React Frontend Development Certification. Duration: 27 weeks. August 17, 2022',
     technologies: 'HTML, CSS, Javascript, ReactJS, Firebase, Bootstrap, SASS, SEO',
-    imageUrl: 'src/assets/certificate/certificadoreactjs.png'
+    imageUrl: certificadoreactjs
   },
   {
     id: 2,
     title: 'Web Development Certification',
     description: 'Certification in Web Design and Development. Duration: 45 hours over 12 weeks. February 19, 2022',
     technologies: 'Search Engine Optimization (SEO), CSS, SCSS, Bootstrap, SASS, Flexbox, HTML, JavaScript',
-    imageUrl: 'src/assets/certificate/certificadodesarrolloweb.png'
+    imageUrl: certificadodesarrolloweb
   },
   {
     id: 3,
     title: 'JavaScript Certification',
     description: 'Certification in Modern JavaScript, including ES6+ and Advanced Techniques. Duration: 40.5 hours over 8 weeks. April 23, 2022',
     technologies: 'JavaScript ES6+',
-    imageUrl: 'src/assets/certificate/certificadojavascript.png'
+    imageUrl: certificadojavascript
   },
   {
     id: 4,
     title: 'Node.js Certification',
     description: 'Certification in JavaScript from Scratch with Node.js. Duration: 12 hours. February 13, 2023',
     technologies: 'JavaScript with Node.js',
-    imageUrl: 'src/assets/certificate/certificadonodejs.jpg'
+    imageUrl: certificadonodejs
   },
   {
     id: 5,
     title: 'Excel Certification',
     description: 'Certification in Excel from Basic to Advanced. Topics include: Data Tables, Relative References, Absolute References, Functions, SUM, COUNT, COUNTA, COUNTIF, VLOOKUP, IF, Pivot Tables, Spreadsheets, Data Entry. Duration: 8 hours. August 15, 2024',
     technologies: 'Excel',
-    imageUrl: 'src/assets/certificate/certificadoexcel.jpg'
+    imageUrl: certificadoexcel
   },
   {
     id: 6,
     title: 'Responsive Web Design Certification',
     description: 'Certification in Responsive Web Design. Duration: 300 hours of work. August 16, 2022',
     technologies: 'Responsive Web Design',
-    imageUrl: 'src/assets/certificate/certificadoresponsive.png'
+    imageUrl: certificadoresponsive
   },
   {
     id: 7,
     title: 'English Certification',
     description: 'Intermediate English Certification. Technical and theoretical knowledge of English. March 6, 2024',
     technologies: 'Intermediate English',
-    imageUrl: 'src/assets/certificate/certificadoingles.png'
+    imageUrl: certificadoingles
   },
   // Agrega más certificaciones según sea necesario
 ];
@@ -60,7 +70,7 @@ const About = () => {
       {/* Sección de introducción */}
       <div className="about-intro">
         <img
-          src="src/assets/img/img7.jpg"
+          src={img7}
           alt="About Me"
           className="about-image"
         />
--- src/components/Portfolio/Portfolio.jsx
+++ src/components/Portfolio/Portfolio.jsx
@@ -1,56 +1,64 @@
-import React from 'react'
-import './Portfolio.css'
+import React from 'react';
+import './Portfolio.css';
+
+// Importación de imágenes
+import img1 from '../../assets/img/img1.png';
+import img2 from '../../assets/img/img2.png';
+import img3 from '../../assets/img/img3.png';
+import img4 from '../../assets/img/img4.png';
+import img5 from '../../assets/img/img5.png';
+import img6 from '../../assets/img/img6.png';
 
 const projects = [
-    {
-      id: 1,
-      title: 'Estudio Inmobiliario',
-      technologies: 'ReactJS, CSS, Javascript, HTML',
-      github: 'https://github.com/SofiaGenchi/mc-estudio-inmobiliario',
-      website: 'https://www.mariainesestudioinmobiliario.com.ar',
-      imageUrl: 'src/assets/img/img1.png'
-    },
-    {
-      id: 2,
-      title: 'Adidas',
-      technologies: 'HTML5, SASS, React, Bootstrap, Firebase, React-Router-Dom, SweetAlert',
-      github: 'https://github.com/SofiaGenchi/adidas',
-      website: 'https://adidas-zeta.vercel.app/category/camisetas',
-      imageUrl: 'src/assets/img/img2.png'
-    },
-    {
-      id: 3,
-      title: 'Generador de QR',
-      technologies: 'HTML, CSS, Javascript',
-      github: 'https://github.com/SofiaGenchi/QRcode',
-      website: 'https://sofiagenchi.github.io/QRcode/',
-      imageUrl: 'src/assets/img/img3.png'
-    },
-    {
-      id: 4,
-      title: 'Gradient Color Generator',
-      technologies: 'HTML, CSS, JavaScript',
-      github: 'https://github.com/SofiaGenchi/Gradient-Color-Generator',
-      website: 'https://gradient-color-generator-seven.vercel.app/',
-      imageUrl: 'src/assets/img/img4.png'
-    },
-    {
-      id: 5,
-      title: 'AI Image Generator',
-      technologies: 'HTML, CSS, Javascript, AI ChatGPT API',
-      github: 'https://github.com/SofiaGenchi/AI-Image-Generation',
-      website: 'https://ai-image-generation-rho.vercel.app/',
-      imageUrl: 'src/assets/img/img5.png'
-    },
-    {
-      id: 6,
-      title: 'Calculador de propina',
-      technologies: 'MySQL, Express',
-      github: 'https://github.com/SofiaGenchi/tip-calculator',
-      website: 'https://sofiagenchi.github.io/tip-calculator/',
-      imageUrl: 'src/assets/img/img6.png'
-    }
-  ];
+  {
+    id: 1,
+    title: 'Estudio Inmobiliario',
+    technologies: 'ReactJS, CSS, Javascript, HTML',
+    github: 'https://github.com/SofiaGenchi/mc-estudio-inmobiliario',
+    website: 'https://www.mariainesestudioinmobiliario.com.ar',
+    imageUrl: img1,
+  },
+  {
+    id: 2,
+    title: 'Adidas',
+    technologies: 'HTML5, SASS, React, Bootstrap, Firebase, React-Router-Dom, SweetAlert',
+    github: 'https://github.com/SofiaGenchi/adidas',
+    website: 'https://adidas-zeta.vercel.app/category/camisetas',
+    imageUrl: img2,
+  },
+  {
+    id: 3,
+    title: 'Generador de QR',
+    technologies: 'HTML, CSS, Javascript',
+    github: 'https://github.com/SofiaGenchi/QRcode',
+    website: 'https://sofiagenchi.github.io/QRcode/',
+    imageUrl: img3,
+  },
+  {
+    id: 4,
+    title: 'Gradient Color Generator',
+    technologies: 'HTML, CSS, JavaScript',
+    github: 'https://github.com/SofiaGenchi/Gradient-Color-Generator',
+    website: 'https://gradient-color-generator-seven.vercel.app/',
+    imageUrl: img4,
+  },
+  {
+    id: 5,
+    title: 'AI Image Generator',
+    technologies: 'HTML, CSS, Javascript, AI ChatGPT API',
+    github: 'https://github.com/SofiaGenchi/AI-Image-Generation',
+    website: 'https://ai-image-generation-rho.vercel.app/',
+    imageUrl: img5,
+  },
+  {
+    id: 6,
+    title: 'Calculador de propina',
+    technologies: 'MySQL, Express',
+    github: 'https://github.com/SofiaGenchi/tip-calculator',
+    website: 'https://sofiagenchi.github.io/tip-calculator/',
+    imageUrl: img6,
+  }
+];
 
 const Portfolio = () => {
   return (
@@ -62,8 +70,7 @@ const Portfolio = () => {
             <div className="portfolio-image" style={{ backgroundImage: `url(${project.imageUrl})` }}>
               <div className="portfolio-overlay">
                 <p>{project.title}</p>
-                <p>{project.description}</p>
-                <p>Technologies: {project.technologies}</p>
+                <p>{project.technologies}</p>
                 <a href={project.github} target="_blank" rel="noopener noreferrer" className="github-link">/&#62;</a>
                 <a href={project.website} target="_blank" rel="noopener noreferrer" className="website-link">www</a>
               </div>
@@ -73,7 +80,7 @@ const Portfolio = () => {
         ))}
       </div>
     </div>
-  )
+  );
 }
 
-export default Portfolio
+export default Portfolio;
