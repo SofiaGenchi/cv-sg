9ec92473d9fe3a433ecbaff1315efd69705d0e98
SofiaGenchi <sofigen16@gmail.com>
2024-09-04 23:07:01 -0300

    terminando con el componente about

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -4,24 +4,52 @@ import './About.css'; // Asegúrate de que el archivo CSS esté en la misma carp
 const certifications = [
   {
     id: 1,
-    title: 'Certificación en React',
-    description: 'Certificación avanzada en ReactJS, cubriendo desde conceptos básicos hasta técnicas avanzadas.',
-    technologies: 'ReactJS, Hooks, Redux',
-    imageUrl: 'src/assets/img/cert1.png'
+    title: 'React Frontend Development Program',
+    description: 'React Frontend Development Certification. Duration: 27 weeks. August 17, 2022',
+    technologies: 'HTML, CSS, Javascript, ReactJS, Firebase, Bootstrap, SASS, SEO',
+    imageUrl: 'src/assets/certificate/certificadoreactjs.png'
   },
   {
     id: 2,
-    title: 'Certificación en CSS',
-    description: 'Certificación en diseño y maquetación con CSS, incluyendo Flexbox y Grid.',
-    technologies: 'CSS3, Flexbox, Grid',
-    imageUrl: 'src/assets/img/cert2.png'
+    title: 'Web Development Certification',
+    description: 'Certification in Web Design and Development. Duration: 45 hours over 12 weeks. February 19, 2022',
+    technologies: 'Search Engine Optimization (SEO), CSS, SCSS, Bootstrap, SASS, Flexbox, HTML, JavaScript',
+    imageUrl: 'src/assets/certificate/certificadodesarrolloweb.png'
   },
   {
     id: 3,
-    title: 'Certificación en JavaScript',
-    description: 'Certificación en JavaScript moderno, incluyendo ES6+ y técnicas avanzadas.',
-    technologies: 'JavaScript ES6+, Asíncrono',
-    imageUrl: 'src/assets/img/cert3.png'
+    title: 'JavaScript Certification',
+    description: 'Certification in Modern JavaScript, including ES6+ and Advanced Techniques. Duration: 40.5 hours over 8 weeks. April 23, 2022',
+    technologies: 'JavaScript ES6+',
+    imageUrl: 'src/assets/certificate/certificadojavascript.png'
+  },
+  {
+    id: 4,
+    title: 'Node.js Certification',
+    description: 'Certification in JavaScript from Scratch with Node.js. Duration: 12 hours. February 13, 2023',
+    technologies: 'JavaScript with Node.js',
+    imageUrl: 'src/assets/certificate/certificadonodejs.jpg'
+  },
+  {
+    id: 5,
+    title: 'Excel Certification',
+    description: 'Certification in Excel from Basic to Advanced. Topics include: Data Tables, Relative References, Absolute References, Functions, SUM, COUNT, COUNTA, COUNTIF, VLOOKUP, IF, Pivot Tables, Spreadsheets, Data Entry. Duration: 8 hours. August 15, 2024',
+    technologies: 'Excel',
+    imageUrl: 'src/assets/certificate/certificadoexcel.jpg'
+  },
+  {
+    id: 6,
+    title: 'Responsive Web Design Certification',
+    description: 'Certification in Responsive Web Design. Duration: 300 hours of work. August 16, 2022',
+    technologies: 'Responsive Web Design',
+    imageUrl: 'src/assets/certificate/certificadoresponsive.png'
+  },
+  {
+    id: 7,
+    title: 'English Certification',
+    description: 'Intermediate English Certification. Technical and theoretical knowledge of English. March 6, 2024',
+    technologies: 'Intermediate English',
+    imageUrl: 'src/assets/certificate/certificadoingles.png'
   },
   // Agrega más certificaciones según sea necesario
 ];
@@ -44,7 +72,7 @@ const About = () => {
 
       {/* Sección de certificaciones y conocimientos */}
       <div className="about-certifications">
-        <h3>Certificaciones y Conocimientos</h3>
+        <h3>Certifications and Skills</h3>
         <div className="certifications-grid">
           {certifications.map(certification => (
             <div className="certification-item" key={certification.id}>
