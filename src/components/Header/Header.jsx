58dba41c59a5b92340d9b83df1b9c851088114a9
SofiaGenchi <sofigen16@gmail.com>
2024-09-17 20:15:09 -0300

    haciendo modificaciones, dando nuevos estilos

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,19 +1,28 @@
-import React from 'react'
-import './Header.css'
-import SocialMedia from '../SocialMedia/SocialMedia'
+import React from "react";
+import "./Header.css";
+import SocialMedia from "../SocialMedia/SocialMedia";
 
 const Header = () => {
   return (
-    <header id='header' className='header'>
+    <header id="header" className="header">
       <SocialMedia />
-        <div className='header-content'>
-        <div className='header-left'>
-            <h1>Web Developer</h1>
-            <p>I have experience in HTML, CSS, JavaScript, React and MySQL.</p>
+      <div className="header-content">
+        <div className="header-left">
+          <div class="terminal-loader">
+            <div class="terminal-header">
+              <div class="terminal-title">Welcome!</div>
+              <div class="terminal-controls">
+                <div class="control close"></div>
+                <div class="control minimize"></div>
+                <div class="control maximize"></div>
+              </div>
+            </div>
+            <div class="text">I'm a Web Developer...</div>
+          </div>
         </div>
-        </div>
+      </div>
     </header>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
